c270c2a8a9badc56596a1721eb8c3479be4ef25e
Soapiane <Ls_yekene@esi.dz>
2023-12-31 21:44:34 +0100

    Architectural midifications

---
+++ Core/Shared/DatabaseOperations.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from Core.Environment.databaseServiceEnv import DATABASE_API_URL, DATABASE_SERVICE_API_KEY
+class Database:
+    @staticmethod
+    def createUser(user):
+        response = requests.post(
+            url=DATABASE_API_URL + "/accounts/create",
+            headers={
+                "account_type": "user",
+                "x-api-key": DATABASE_SERVICE_API_KEY
+            },
+            data=json.dumps(user),
+        )
+        return response.json()
+
+    @staticmethod
+    def getAccountsWithFilter(user):
+        response = requests.get(
+            url=DATABASE_API_URL + "/accounts/getAccounts",
+            headers={
+                "x-api-key": DATABASE_SERVICE_API_KEY
+            },
+            data=json.dumps(user),
+        )
+        return response.json()
