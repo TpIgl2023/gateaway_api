ee14515d2636fdd7144d60d33bc16bea89128560
Fares Mezenner <faresmezenner@gmail.com>
2024-01-03 08:30:14 +0100

    upload article service now indexs the article

--- Services/articleServices.py
+++ Services/articleServices.py
@@ -1,6 +1,7 @@
 from Core.Exceptions.databaseException import DatabaseException
 from Models.Article import Article
 from Core.Configuration.databaseConfiguration import articlesDatabaseClient
+from Services.elasticsearchServices import index_article, remove_article_from_index, search_articles
 
 
 def _fields_rename_for_database(article: dict):
@@ -30,9 +31,12 @@ async def _upload_article_to_database(article: Article):
     return response.json()["article"]
 
 
-# TODO: complete this function
+# TODO: add uploading pdf to the storage (check if necessary)
 async def upload_article(article: Article):
     # Upload the article to the database
     uploaded_article = await _upload_article_to_database(article)
+
+    # Index the article
+    index_article(article, uploaded_article["id"])
 
     return uploaded_article
