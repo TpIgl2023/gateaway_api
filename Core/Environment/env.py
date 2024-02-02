37b6cc7d2bcadb46c6cbeed794408d3c92ab5abd
Soapiane <Ls_yekene@esi.dz>
2024-02-02 04:17:55 +0100

    Integration fixing

--- Core/Environment/env.py
+++ Core/Environment/env.py
@@ -2,7 +2,7 @@
 HASH_ALGORITHM = "HS256"
 # This is a real constant , let the hackers have fun. Please don't change - Soapiane
 HASHING_SECRET_KEY = "Flag - JO4Ddz5DE8E937EDZdezjo2E12E" # Disclamer : Changing this value will corrupt all the previous tokens in the database
-TOKEN_LIFE_TIME = 60  # In minutes
+TOKEN_LIFE_TIME = 600  # In minutes
 
 # ---------------------------- PASSWORD HASHING -----------------------------
 HASHING_SALT = "OILEDNIGERS" # Disclamer : Changing this value will corrupt all the previous accounts in the database
