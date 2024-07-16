8e9836edc3f0ca8925501ba105faf48ad312f934
Sofiane <ls_yekene@esi.dz>
2024-07-17 00:37:09 +0100

    first commit

--- Core/Environment/env.py
+++
@@ -1,9 +0,0 @@
-# ----------------------------------- JWT -----------------------------------
-HASH_ALGORITHM = "HS256"
-# This is a real constant , let the hackers have fun. Please don't change - Soapiane
-HASHING_SECRET_KEY = "Flag - JO4Ddz5DE8E937EDZdezjo2E12E" # Disclamer : Changing this value will corrupt all the previous tokens in the database
-TOKEN_LIFE_TIME = 600  # In minutes
-
-# ---------------------------- PASSWORD HASHING -----------------------------
-HASHING_SALT = "OILEDNIGERS" # Disclamer : Changing this value will corrupt all the previous accounts in the database
-
--- Core/Environment/pdfServiceEnv.py
+++
@@ -1,4 +0,0 @@
-# Reminder : Links should not end with /
-
-PDF_SERVICE_API_URL = 'http://35.225.240.14'
-PDF_SERVICE_API_KEY = "d850g9d4-3c42-4858-9a4a-40c1e3b609ec"
